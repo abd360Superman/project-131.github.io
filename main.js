ae12d34e44d9087d85389abea355aa1b161a5434
abd360superman <darshchoudhary171@gmail.com>
2021-07-23 19:30:08 +0530

    Custom commit name

---
+++ main.js
@@ -0,0 +1,26 @@
+homeHtml = '<button class="btn btn-info home-btn" onclick="loadBedroom()">Bedroom</button><button class="btn btn-info home-btn" onclick="loadLiving()">Living Room</button><button class="btn btn-info home-btn" onclick="loadFruits()">Fruits</button><button class="btn btn-info home-btn" onclick="loadBottles()">Bottles</button><button class="btn btn-info home-btn" onclick="loadDesk()">Desk</button>';
+
+function loadHome() {
+    document.getElementById('dynamically-add').innerHTML = ''
+    document.getElementById('dynamically-add').innerHTML = homeHtml
+}
+
+function loadBedroom() {
+
+}
+
+function loadLiving() {
+
+}
+
+function loadFruits() {
+
+}
+
+function loadBottles() {
+
+}
+
+function loadDesk() {
+    
+}
